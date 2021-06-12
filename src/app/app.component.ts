0245481422e8574ba7bacb71fb09635d5d93fa10
smritissh <smriti.ssh@gmail.com>
2021-06-12 17:45:58 +0530

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  parentComponent(data) {
+    console.warn(data);
+    
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @Output() parentComponent: EventEmitter<any> = new EventEmitter();
+  constructor() {}
 
   ngOnInit() {
+    this.parentComponent.emit('hello');
   }
-
 }
