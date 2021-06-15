fd89f2e044538d85deb8411e4be7334e60b59e40
smritissh <smriti.ssh@gmail.com>
2021-06-15 18:18:40 +0530

    2 files modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -25,7 +25,7 @@ export class ProductComponent implements OnInit {
 
   onCartRemove() {
     if (this.c === 0) {
-      alert('Cart is empty');
+      alert('No Items in Cart');
     } else {
       this.c = this.c - 1;
       this.cartItem.sendCartItem(this.c);
