498cd4ac4f56dc1428c95f9217f305d319ea065f
smritissh <smriti.ssh@gmail.com>
2021-06-16 20:17:31 +0530

    1 file modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,14 +7,15 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {
-    registerationForm = new FormGroup({
+  registerationForm: FormGroup;
+  constructor() {
+    this.registerationForm = new FormGroup({
       name: new FormControl('smriti'),
       mobileNo: new FormControl(''),
       email: new FormControl(''),
       password: new FormControl('')
     });
   }
+
+  ngOnInit() {}
 }
